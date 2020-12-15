0f1998175b64b724ee57d9aba70fd8ba757db782
alefka007 <alefka007@gmail.com>
2020-12-15 21:55:49 +0300

    Учебный коммит

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+    $('.top-slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                }
+            },
+            {
+                breakpoint: 320,
+                settings: {
+                }
+            }
+        ]
+    });
+});
