eae3521780fb0bc31d02625416499fce8707eea5
alefka007 <alefka007@gmail.com>
2021-02-08 21:06:37 +0300

    Дополнения

--- js/index.js
+++ js/index.js
@@ -4,9 +4,18 @@ Swiper.use([Navigation, Pagination]);
 import 'swiper/swiper-bundle.min.css';
 const swiper = new Swiper();
 
-var mySwiper = new Swiper('.swiper-container', {
+var Swiper1 = new Swiper('.swiper-container', {
     direction: 'horizontal',
     loop: true,
+    pagination: {
+    el: '.swiper-pagination',
+    }
+})
+
+var Swiper2 = new Swiper('.swiper-container2', {
+    direction: 'horizontal',
+    type: 'bullets',
+    clickable: true,
     pagination: {
     el: '.swiper-pagination',
     }
